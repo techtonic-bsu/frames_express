ebc17e800bb88c958f156c55705cb54169b38b8a
mariushernandez <MariusJacob.Hernandez@asurion.com>
2024-07-20 09:20:05 +0800

    boiler plate for express

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// import { dotnenv } from 'dotenv';
+// import express from 'express';
+// import mongoose from 'mongoose';
+
+// dotnenv.config()
+// const app=express()
+// const cors=require('cors')
+// const jwt=require('jsonwebtoken')
+
+// app.use(express.json())
+// app.use(express.static('public'))
+
+
+// mongoose.connect(process.env.PORT, ()=>{
+//     console.log("now to connected to DB")
+// }).catch((e)=>{
+//     console.log("error")
+// })
