3eef0958fce6bed44712c793f8261b57dcb5de9a
mariushernandez <MariusJacob.Hernandez@asurion.com>
2024-07-20 15:35:15 +0800

    signup connection with mongodb done with routes also done

--- src/api/signup/SignupController.js
+++ src/api/signup/SignupController.js
@@ -3,8 +3,8 @@ const mongoose=require('mongoose')
 
 const createUser=async(req, res)=>{
     try {
-        const signup= await create(req.body)
-        res.status(200).json({message:"success post"})
+        const signup= await Signup.create(req.body)
+        res.status(200).json({message:signup})
     } catch (e) {
         res.status(400).json({error:e.message})
     }
